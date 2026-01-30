5e0e33dcfad456bab1961e5a2f14a701fbef6d64
Hank <hongkunw2002@gmail.com>
2026-01-30 14:27:20 +0800

    123

--- src/lib/logistics-constants.ts
+++ src/lib/logistics-constants.ts
@@ -4,13 +4,8 @@
  */
 
 // 物流公司列表
+// 物流公司列表
 export const LOGISTICS_COMPANIES = [
   { code: "SF", name: "顺丰速运" },
-  { code: "YTO", name: "圆通速递" },
-  { code: "ZTO", name: "中通快递" },
-  { code: "STO", name: "申通快递" },
-  { code: "YD", name: "韵达快递" },
-  { code: "JD", name: "京东物流" },
-  { code: "EMS", name: "EMS" },
 ];
 
