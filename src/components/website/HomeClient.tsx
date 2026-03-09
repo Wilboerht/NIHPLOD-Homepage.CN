ff185c60253e4c9793e88d5959bc4ecb3f1c5f87
Hank <hongkunw2002@gmail.com>
2026-03-09 16:43:46 +0800

    1

--- src/components/website/HomeClient.tsx
+++ src/components/website/HomeClient.tsx
@@ -405,7 +405,7 @@ export default function HomeClient({ content: _content }: HomeClientProps) {
 
                   {/* 按钮组 - 增加触压反馈 */}
                   <m.div
-                    className="button-group mt-12 md:mt-16 flex gap-6"
+                    className="button-group mt-14 md:mt-20 flex gap-6"
                     initial={{ opacity: 0, y: 20 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 1.2, delay: 1.2 }}
