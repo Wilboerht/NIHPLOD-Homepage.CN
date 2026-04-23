97d1155a370b7494f16558edbb7fa06109e7f6e6
Hank <hongkunw2002@gmail.com>
2026-04-23 19:23:57 +0800

    1

--- src/lib/upload.ts
+++ src/lib/upload.ts
@@ -42,7 +42,7 @@ export const uploadConfig = {
     "image/gif",
     "application/pdf", // 支持 PDF
   ],
-  uploadDir: "uploads",
+  uploadDir: "public/uploads",
   quality: 80, // WebP 质量
   maxWidth: 2000, // 最大宽度
   maxHeight: 2000, // 最大高度
