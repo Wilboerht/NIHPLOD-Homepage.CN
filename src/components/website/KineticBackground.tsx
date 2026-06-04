6f7f3959044fb50d1dc42427757c1068ad5476d8
Hank <hongkunw2002@gmail.com>
2026-06-05 02:55:43 +0800

    1

--- src/components/website/KineticBackground.tsx
+++ src/components/website/KineticBackground.tsx
@@ -97,7 +97,7 @@ export function KineticBackground() {
             <div className="kinetic-watermark">
                 {/* PC 端水印 */}
                 <Image
-                    src="/images/watermark.webp"
+                    src="/images/N-web.svg"
                     alt="Watermark PC"
                     width={2800}
                     height={800}
