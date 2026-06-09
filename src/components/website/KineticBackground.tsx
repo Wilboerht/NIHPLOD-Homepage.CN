1873615e48836a58a33a8cd851b378877b3628ea
Hank <hongkunw2002@gmail.com>
2026-06-09 18:58:51 +0800

    1

--- src/components/website/KineticBackground.tsx
+++ src/components/website/KineticBackground.tsx
@@ -96,7 +96,7 @@ export function KineticBackground() {
             <div className="kinetic-dot-pattern" />
             <div className="kinetic-watermark">
                 {/* PC 端水印 */}
-                <div className="hidden md:block relative opacity-[0.65]">
+                <div className="hidden md:block relative opacity-[0.15]">
                     <Image
                         src="/images/N-web.svg"
                         alt="Watermark PC"
