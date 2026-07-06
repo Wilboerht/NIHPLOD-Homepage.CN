fb0b9d7aa00b5e4c6e78aafcc9138454de202995
Hank <hongkunw2002@gmail.com>
2026-07-06 17:26:41 +0800

    1

--- src/app/(website)/privacy/PrivacyContent.tsx
+++ src/app/(website)/privacy/PrivacyContent.tsx
@@ -317,7 +317,7 @@ export function PrivacyContent() {
       </div>
 
       {/* Page Footer */}
-      <footer className="mt-16 border-t border-zinc-100">
+      <footer className="mt-24 border-t border-zinc-100">
         <div className="container mx-auto px-6 md:px-8 lg:px-12 xl:px-16 py-8">
           <p className="text-xs text-zinc-400">
             &copy; {new Date().getFullYear()} NIHPLOD. All Rights Reserved.
