4a71ef00b351b655a78a61aff75b828830d3420c
Hank <hongkunw2002@gmail.com>
2026-07-17 14:27:25 +0800

    1

--- src/components/website/HomeClient.tsx
+++ src/components/website/HomeClient.tsx
@@ -318,18 +318,10 @@ export default function HomeClient({ content: _content }: HomeClientProps) {
 
           {/* 波浪背景 - 仅桌面端渲染，CSS 浮动动画 */}
           <div className="wave-container pointer-events-none absolute bottom-0 left-0 right-0 z-0 hidden lg:block">
-            <svg
-              className="wave wave-1"
-              viewBox="0 0 1200 120"
-              preserveAspectRatio="none"
-            >
+            <svg className="wave wave-1" viewBox="0 0 1200 120" preserveAspectRatio="none">
               <path d="M0,60 C150,110 350,10 500,60 C650,110 850,10 1000,60 C1150,110 1350,10 1500,60" />
             </svg>
-            <svg
-              className="wave wave-2"
-              viewBox="0 0 1200 120"
-              preserveAspectRatio="none"
-            >
+            <svg className="wave wave-2" viewBox="0 0 1200 120" preserveAspectRatio="none">
               <path d="M0,40 C200,90 400,0 600,40 C800,80 1000,0 1200,40" />
             </svg>
           </div>
