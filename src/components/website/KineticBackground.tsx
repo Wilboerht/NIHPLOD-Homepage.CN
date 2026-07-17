9d084c56b0cf2ec0ce7e795b53d7128d6afa0c2c
Hank <hongkunw2002@gmail.com>
2026-07-17 13:53:52 +0800

    1

--- src/components/website/KineticBackground.tsx
+++ src/components/website/KineticBackground.tsx
@@ -230,7 +230,7 @@ export function KineticBackground() {
         >
           <div
             className="kinetic-name"
-            style={{ marginBottom: "12px", lineHeight: "1.3", fontSize: "1.8rem", fontWeight: 300 }}
+            style={{ marginBottom: "12px", lineHeight: "1.3", fontSize: "1.8rem", fontWeight: 500 }}
           >
             更少步骤
             <br />
@@ -302,7 +302,7 @@ export function KineticBackground() {
           ref={(el) => addCellRef(el, 4)}
           className="kinetic-cell kinetic-text-cell kinetic-cell-orange kinetic-cell-reverse no-hover-effect"
         >
-          <div className="kinetic-name" style={{ fontSize: "1.8rem", fontWeight: 300 }}>
+          <div className="kinetic-name" style={{ fontSize: "1.8rem", fontWeight: 500 }}>
             逆转时光
           </div>
           <div
