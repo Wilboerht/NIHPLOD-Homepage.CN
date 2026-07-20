551b9630996f714ad10497df7199f772a6f224a1
Hank <hongkunw2002@gmail.com>
2026-07-20 16:50:36 +0800

    1

--- src/app/(website)/privacy/PrivacyContent.tsx
+++ src/app/(website)/privacy/PrivacyContent.tsx
@@ -184,7 +184,7 @@ const sections = sectionOrder.map((id) => ({
 
 export function PrivacyContent() {
   return (
-    <div className="min-h-screen animate-fade-in bg-[#fefcf8] pb-0 pt-16 md:pt-24">
+    <div className="min-h-dvh animate-fade-in bg-[#fefcf8] pt-16 md:pt-24">
       {/* Header Section */}
       <div className="container mx-auto mb-8 px-6 md:mb-16 md:px-8 lg:px-12 xl:px-16">
         <div className="max-w-4xl">
