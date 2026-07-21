83a701d1fc8da367cd3c32fbba891dcdd579e8a6
Hank <hongkunw2002@gmail.com>
2026-07-21 17:26:11 +0800

    1

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,11 +2,7 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "./prisma/schema.prisma",
-  datasource: {
-    url: process.env.DATABASE_URL!,
-  },
   migrations: {
-    path: "prisma/migrations",
-    seed: "tsx prisma/seed.ts",
+    url: process.env.DATABASE_URL!,
   },
 });
