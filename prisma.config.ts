b7270d9183fba9048f4c3097250a9e7ed5b09e26
Hank <hongkunw2002@gmail.com>
2026-07-21 18:05:33 +0800

    1

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,4 +2,8 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "./prisma/schema.prisma",
+  datasource: {
+    provider: "postgresql",
+    url: "env://DATABASE_URL",
+  },
 });
